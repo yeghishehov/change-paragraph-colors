dc89e661ce4b46dd0035a73add2a663e2a3b961b
yeghishehov <yeghishehov.1@gmail.com>
2020-01-06 15:36:18 +0400

    start creating color

---
+++ src/Color.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
+    render() {
+        return (
+            <input type="text"/>
+        )
+    }
+
+
+}
