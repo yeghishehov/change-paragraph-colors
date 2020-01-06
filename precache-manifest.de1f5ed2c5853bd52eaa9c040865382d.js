40cbe2b9d7310d92794de529d8f8840a713aa901
yeghishehov <yeghishehov.1@gmail.com>
2020-01-06 22:35:39 +0400

    Updates

---
+++ precache-manifest.de1f5ed2c5853bd52eaa9c040865382d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc05031d7a574f5d8c87316c07b5135a",
+    "url": "/change-paragraph-colors/index.html"
+  },
+  {
+    "revision": "f8fed76956882365e57c",
+    "url": "/change-paragraph-colors/static/js/2.13fd7f31.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/change-paragraph-colors/static/js/2.13fd7f31.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d1b47f1dc1a479bdea2",
+    "url": "/change-paragraph-colors/static/js/main.06e98e3b.chunk.js"
+  },
+  {
+    "revision": "8a713bc275e7b643a414",
+    "url": "/change-paragraph-colors/static/js/runtime-main.817bf337.js"
+  }
+]);
